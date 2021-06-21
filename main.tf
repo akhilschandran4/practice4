0a24fe66bd186422d4af24f2d6b17589adfca7f1
akhilschandran4 <akhilschandran4@gmail.com>
2021-06-21 22:12:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = "3457d5d0-1535-40c6-85fb-cb1919104698"
-
+  
  }
 
 resource "azurerm_resource_group" "test" {
@@ -20,7 +20,7 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_storage_account" "test" {
-  name = ""
+  name = "mystorageaccount"
   resource_group_name = azurerm_resource_group.test.name
   location = azurerm_resource_group.test.location
   account_replication_type = "GRS"
